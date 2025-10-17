bfda505de5b28832a954941ddbc1c6433035545c
sqenaah <sarkis05.08.2008@gmail.com>
2025-10-17 13:15:25 +0400

    Update __main__.py

--- ShrutiMusic/__main__.py
+++ ShrutiMusic/__main__.py
@@ -1,25 +1,4 @@
 utf-8utf-8
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 importasyncio
 importimportlib
 frompyrogramimportidle
@@ -37,35 +16,14 @@ fromconfigimportBANNED_USERS
 COMMANDS=[
 BotCommand("start","🚀 Start bot"),
 BotCommand("help","❓ Help menu and Many More Management Commands"),
-BotCommand("ping","📡 Ping and system stats"),
 BotCommand("play","🎵 Start streaming the requested track"),
-BotCommand("vplay","📹 Start video streaming"),
-BotCommand("playrtmps","📺 Play Live Video"),
-BotCommand("playforce","⚠️ Force play audio track"),
-BotCommand("vplayforce","⚠️ Force play video track"),
-BotCommand("pause","⏸ Pause the stream"),
-BotCommand("resume","▶️ Resume the stream"),
-BotCommand("skip","⏭ Skip the current track"),
-BotCommand("end","🛑 End the stream"),
-BotCommand("stop","🛑 Stop the stream"),
 BotCommand("queue","📄 Show track queue"),
-BotCommand("auth","➕ Add a user to auth list"),
-BotCommand("unauth","➖ Remove a user from auth list"),
-BotCommand("authusers","👥 Show list of auth users"),
 BotCommand("cplay","📻 Channel audio play"),
-BotCommand("cvplay","📺 Channel video play"),
-BotCommand("cplayforce","🚨 Channel force audio play"),
-BotCommand("cvplayforce","🚨 Channel force video play"),
-BotCommand("channelplay","🔗 Connect group to channel"),
-BotCommand("loop","🔁 Enable/disable loop"),
-BotCommand("stats","📊 Bot stats"),
-BotCommand("shuffle","🔀 Shuffle the queue"),
 BotCommand("seek","⏩ Seek forward"),
-BotCommand("seekback","⏪ Seek backward"),
-BotCommand("song","🎶 Download song (mp3/mp4)"),
+BotCommand("seekback","⏪ Seek backward")
 BotCommand("speed","⏩ Adjust audio playback speed (group)"),
 BotCommand("cspeed","⏩ Adjust audio speed (channel)"),
-BotCommand("tagall","📢 Tag everyone"),
+BotCommand("tag","📢 Tag everyone"),
 ]
 
 asyncdefsetup_bot_commands():
@@ -73,10 +31,10 @@ asyncdefsetup_bot_commands():
 try:
 
         awaitapp.set_bot_commands(COMMANDS)
-LOGGER("ShrutiMusic").info("Bot commands set successfully!")
+LOGGER("NuvielMusic").info("Bot commands set successfully!")
 
 exceptExceptionase:
-        LOGGER("ShrutiMusic").error(f"Failed to set bot commands: {str(e)}")
+        LOGGER("NuvielMusic").error(f"Failed to set bot commands: {str(e)}")
 
 asyncdefinit():
     if(
@@ -107,9 +65,9 @@ awaitapp.start()
 awaitsetup_bot_commands()
 
 forall_moduleinALL_MODULES:
-        importlib.import_module("ShrutiMusic.plugins"+all_module)
+        importlib.import_module("NuvielMusic.plugins"+all_module)
 
-LOGGER("ShrutiMusic.plugins").info("Successfully Imported Modules...")
+LOGGER("NuvielMusic.plugins").info("Successfully Imported Modules...")
 
 awaituserbot.start()
 awaitNand.start()
@@ -117,7 +75,7 @@ awaitNand.start()
 try:
         awaitNand.stream_call("https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4")
 exceptNoActiveGroupCall:
-        LOGGER("ShrutiMusic").error(
+        LOGGER("NuvielMusic").error(
 "Please turn on the videochat of your log group\channel.\n\nStopping Bot..."
 )
 exit()
@@ -126,7 +84,7 @@ except:
 
 awaitNand.decorators()
 
-LOGGER("ShrutiMusic").info(
+LOGGER("NuvielMusic").info(
 "\x53\x68\x72\x75\x74\x69\x20\x4d\x75\x73\x69\x63\x20\x53\x74\x61\x72\x74\x65\x64\x20\x53\x75\x63\x63\x65\x73\x73\x66\x75\x6c\x6c\x79\x2e\x0a\x0a\x44\x6f\x6e\x27\x74\x20\x66\x6f\x72\x67\x65\x74\x20\x74\x6f\x20\x76\x69\x73\x69\x74\x20\x40\x53\x68\x72\x75\x74\x69\x42\x6f\x74\x73"
 )
 
@@ -134,19 +92,7 @@ awaitidle()
 
 awaitapp.stop()
 awaituserbot.stop()
-LOGGER("ShrutiMusic").info("Stopping Shruti Music Bot...🥺")
+LOGGER("NuvielMusic").info("Stopping Shruti Music Bot...🥺")
 
 if__name__=="__main__":
     asyncio.get_event_loop().run_until_complete(init())
-
-
-
-
-
-
-
-
-
-
-
-
