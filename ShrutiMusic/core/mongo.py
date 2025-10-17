cb949c2e5b648522584b08bd56e009618bd41565
sqenaah <sarkis05.08.2008@gmail.com>
2025-10-17 13:38:34 +0400

    Update mongo.py

--- ShrutiMusic/core/mongo.py
+++ ShrutiMusic/core/mongo.py
@@ -1,25 +1,4 @@
 utf-8utf-8
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 frommotor.motor_asyncioimportAsyncIOMotorClient
 
 fromconfigimportMONGO_DB_URI
@@ -34,15 +13,3 @@ LOGGER(__name__).info("Connected to your Mongo Database.")
 except:
     LOGGER(__name__).error("Failed to connect to your Mongo Database.")
 exit()
-
-
-
-
-
-
-
-
-
-
-
-
